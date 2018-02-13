16c3cac08b8044d0920d3bda3817d512518d496e
Leroy Salih <Leroy.Salih@gmail.com>
2018-02-13 11:47:09 +0300

    Got StackView in Playground working...

--- StackView/StackView.playground/Contents.swift
+++ StackView/StackView.playground/Contents.swift
@@ -7,43 +7,46 @@ var canvas = UIView(frame: CGRect(x: 0, y: 0, width: 400, height: 300))
 canvas.backgroundColor = UIColor.red
 canvas.translatesAutoresizingMaskIntoConstraints = false
 
-var stackView = UIStackView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+var stackView = UIStackView(frame: CGRect(x: 0, y: 0, width: 00, height: 00))
+stackView.translatesAutoresizingMaskIntoConstraints = false
+
 stackView.backgroundColor = .red
 
 var label1 = UILabel()
 label1.text = "Red"
 label1.backgroundColor = .red
-label1.sizeToFit()
+label1.setContentHuggingPriority(UILayoutPriority(rawValue:250), for: .vertical)
+
 
 
 var label2 = UILabel()
 label2.text = "Yellow"
 label2.backgroundColor = .yellow
-label2.sizeToFit()
+label1.setContentHuggingPriority(UILayoutPriority(rawValue:250), for: .vertical)
 
 var label3 = UILabel()
 label3.text = "Green"
 label3.backgroundColor = .green
-label3.sizeToFit()
+label1.setContentHuggingPriority(UILayoutPriority(rawValue:249), for: .vertical)
+
 
 stackView.addArrangedSubview(label1)
 stackView.addArrangedSubview(label2)
 stackView.addArrangedSubview(label3)
 
+// StackView Configuration
+stackView.axis = .vertical
+stackView.distribution = .fill
+stackView.spacing = 5
+
 canvas.addSubview(stackView)
 
 NSLayoutConstraint.activate([
-    label1.widthAnchor.constraint(equalToConstant: 50),
-    label2.widthAnchor.constraint(equalToConstant: 50),
-    label3.widthAnchor.constraint(equalToConstant: 50),
-    
-    label1.heightAnchor.constraint(equalToConstant: 50),
-    label2.heightAnchor.constraint(equalToConstant: 50),
-    label3.heightAnchor.constraint(equalToConstant: 50),
-    
+  
     stackView.topAnchor.constraint(equalTo: canvas.topAnchor),
     stackView.leadingAnchor.constraint(equalTo: canvas.leadingAnchor),
     stackView.trailingAnchor.constraint(equalTo: canvas.trailingAnchor)
+    
 ])
 
 
